577d4500801cff686937a1628bf09a45d860dd45
BelovAV <Belov20191>
2019-12-09 09:15:56 +0300

    fix Date

--- src/utils/date.js
+++ src/utils/date.js
@@ -13,15 +13,16 @@ export const DATE = {
     daysLeftNewYear: (value) => {
         let targetDate = new Date(value).getTime();
         let curDate = new Date().getTime();
-        let result = Math.round((curDate - targetDate) / 86400000);
+        let result = Math.abs(Math.round((curDate - targetDate) / 86400000));
+        console.log(result);
         if (result === 1 || result === 21 || result === 31) {
-            return  `До нового года остался  ${Math.abs(result)} день!`;
+            return  `До нового года остался  ${result} день!`;
         } else if (result > 1 && result < 5 || result > 21 && result < 26) {
-            return  `До нового года осталось  ${Math.abs(result)} дня!`;
+            return  `До нового года осталось  ${result} дня!`;
         } else if (result === 0) {
             return  `С Новым годом!`;
         } else {
-            return `До нового года осталось  ${Math.abs(result)} дней!`;
+            return `До нового года осталось  ${result} дней!`;
         }
     }
 }
